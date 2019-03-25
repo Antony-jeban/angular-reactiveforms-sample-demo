007835655a9f284c330fe6ffdff5ebf9e3816b5c
Antony-jeban <antonyjeban@gmail.com>
2019-03-25 16:10:34 +0530

    Added upto form arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+
+function emailValidation(c: AbstractControl): {[key: string]:  boolean} | null{
+  const email = c.get('email')
+  const reenter = c.get('reenteremail')
+  if( email.pristine && reenter.pristine ){
+    return null
+  }
+  if( email.value == reenter.value){
+    return null
+  } else {
+    return { 'match': true }
+  }
+}
+
 
 @Component({
   selector: 'my-app',
@@ -8,31 +22,60 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 })
 export class AppComponent {
   public customerForm: FormGroup
+  emailMessage: string
+  validationmsg: object
+
 
   constructor(private fb: FormBuilder) {
+
   }
   ngOnInit() {
+      this.validationmsg = {
+  required: 'This field is required',
+  email: 'Enter a valid Email',
+  match: 'Email entered does not match'
+}
     this.customerForm = this.fb.group({
       firstname: ['', Validators.required],
       lastname: '',
-      email: ''
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        reenteremail: ['', [Validators.required]]
+      }, {validators: emailValidation}),
+      phnoArray: this.fb.array([ this.createInput() ])
     })
+
+    this.customerForm.get('emailGroup.email').valueChanges.subscribe(
+      (value) => { this.setMessage(value) }
+    )
   }
 
   populateData(){
-    this.customerForm.setValue({
+    this.customerForm.patchValue({
       firstname: "Jeban",
       lastname: "Antony",
-      email: "antonyjeban@gmail.com"
+      emailGroup: {
+        email: "antonyjeban@gmail.com"
+      }
     })
   }
 
-  // private addressMask(value) {
-  //       if(!_.isString(value) || !value.length) {
-  //           return false;
-  //       }
-  //       const totalArr = new Array(value.length);
-  //       const maskArr = _.map(totalArr, () => [/^(?!\s)[a-z0-9A-Z\s\/\\-]*((?!\s).|^)$/]);
-  //       return maskArr;
-  //   }
+setMessage(data: AbstractControl) {
+   this.emailMessage = ''
+   if(((data.touched || data.dirty)) && data.errors) {
+     this.emailMessage = Object.keys(data.errors).map(
+       key => this.emailMessage += this.validationmsg[key]).join('');
+   }
 }
+
+createInput() {
+  return this.fb.group({ phone: '' })
+}
+
+getaddField() {
+  this.customerForm.get('phnoArray')
+}
+
+
+}
+
